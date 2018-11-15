84a5cf86178f5092b9ea2294e2127166a3faf23c
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-15 21:00:24 +0100

    Update 5f.js

--- 5f.js
+++ 5f.js
@@ -1,11 +1,10 @@
 
-		
-		class Student
-		{
-		Student(first, last, id, grades) {
-    this.first = first;
-    this.last = last;
-    this.id = id;
-    this.grades = grades;
+class Student
+{
+Student(first, last, id, grades) {
+this.first = first;
+this.last = last;
+this.id = id;
+this.grades = grades;
 }
-		}	
+}	
