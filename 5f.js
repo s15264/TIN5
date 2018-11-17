b312c141f1de0077bf0581fe9c04cd68679b0a64
s15264 <44864109+s15264@users.noreply.github.com>
2018-11-17 15:02:00 +0100

    Update 5f.js

--- 5f.js
+++ 5f.js
@@ -1,10 +1,26 @@
 
 class Student
 {
-Student(first, last, id, grades) {
+Student(first, last) {
 this.first = first;
 this.last = last;
-this.id = id;
-this.grades = grades;
+
 }
+  
+get fullName(){
+  	return this.first+" "+this.last;
+  }
+}
+  class Student extends Person {
+	constructor(first, last, id, grades) {
+  	super(firstName, lastName);
+  	this.id = id;
+  	this.grades = grades;
+	}
+
+  
 }	
+let Person01 = new Person('John', 'Doe');
+let Student01 = new Student('Jan', 'Kowalski',1, [4,3,3]);
+console.log(Person01);
+console.log(Student01);
